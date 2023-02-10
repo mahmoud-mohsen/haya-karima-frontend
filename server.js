460c84d8b527327c172b3f97276c61608a1bcbe3
Mahmoud Mohsen <mahmoudmohsen335@gmail.com>
2023-02-11 00:31:51 +0200

    update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/first-angular-project'));
+app.use(express.static(__dirname + '/dist/haya-karima'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
